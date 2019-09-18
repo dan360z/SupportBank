afa7b3bec8f5d5d5bd27c64f98160bc80e5d44ce
dlfield <dlfield@c02y8495jgh6.uk.aexp.com>
2019-09-18 14:19:42 +0100

    Added classes

--- src/main/java/training/supportbank/Main.java
+++ src/main/java/training/supportbank/Main.java
@@ -2,7 +2,38 @@ package training.supportbank;
 
 public class Main {
     public static void main(String args[]) {
-        // Your code here!
-        System.out.println("Test!");
+
+
+        class Person {
+            String name;
+            double totalFunds = 0;
+        }
+
+        class Transaction {
+            String date;
+            String narrative;
+            Person to;
+            Person from;
+            double amount;
+        }
+
+
+
+        /*
+        Person jon = new Person();
+        jon.name = "Jon A";
+
+        Person sarah = new Person();
+        sarah.name = "Sarah T";
+
+
+        Transaction lunch = new Transaction();
+        lunch.amount = 10;
+        lunch.to = jon;
+        lunch.from = sarah;
+
+        jon.totalFunds += lunch.amount;
+        sarah.totalFunds -= lunch.amount;*/
+
     }
 }
